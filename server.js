a0b99499c493d0cdf02c21908d3a6fbea3cd1f68
alleemccoy <alleemccoy@hotmail.com>
2021-04-14 18:45:54 -0700

    Completed back-end API task List in Trello, that is half of card 2

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 'use strict';
 
 const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
 const weatherData = require('./data/weather.json');
 
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
 app.get('/weather', (request, response) => {
-  response.send('Looking for weather?');
+  const alldailyForecasts = weatherData.data.map(day => new DailyForecast(day));
+
+
+  response.send(alldailyForecasts);
 });
 
-
+function DailyForecast(day) {
+  this.date = day.datetime;
+  this.description = day.weather.description;
+}
 
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
